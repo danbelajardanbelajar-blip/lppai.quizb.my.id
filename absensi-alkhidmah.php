ba890ad570a08a5eb879ff6b564d9148bffe39d3
zenhkm <zenhkm@gmail.com>
2026-09-04 14:19:38 +0700

    Update max distance for absensi al khidmah to 50 meters

--- absensi-alkhidmah.php
+++ absensi-alkhidmah.php
@@ -75,7 +75,7 @@ include __DIR__ . '/includes/header.php';
 document.addEventListener("DOMContentLoaded", function() {
     const TARGET_LAT = -7.095500;
     const TARGET_LNG = 112.330477;
-    const MAX_DISTANCE_METERS = 40;
+    const MAX_DISTANCE_METERS = 50;
 
     const statusMsg = document.getElementById('status-message');
     const scannerWrapper = document.getElementById('scanner-wrapper');
